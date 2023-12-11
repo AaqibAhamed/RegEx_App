2fdb99e982d58ee68f8ff41efa4e621cfc82c120
ahame <ahame@SP0884>
2023-12-11 11:32:03 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+
+
+
+ string GetRecipient(string message, int position)
+{
+    string pattern = @"@([a-zA-Z0-9_\w-]+)";
+
+    MatchCollection matches = Regex.Matches(message, pattern);
+
+    if (position >= 1 && position <= matches.Count)
+    {
+        return matches[position - 1].Groups[1].Value;
+    }
+
+    else
+    {
+        return string.Empty;
+    }
+}
+
+Console.WriteLine(GetRecipient("asdass @asdasd @dfgfdgfg @goror",5));
